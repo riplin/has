b39213987d1e02952e38131967c1cd2e0e744059
riplin <jeanpaulkogelman@me.com>
2025-07-31 13:46:00 +0200

    Add import defines that can rename namespaces

--- include/has/imports.h
+++ include/has/imports.h
@@ -2,26 +2,50 @@
 
 #pragma once
 
-#define IMPORTNAMESPACEANDTYPE(N, R)        \
-typedef N::R ## _t R ## _t;                 \
-namespace R                                 \
-{                                           \
-    using namespace N::R;                   \
+#define IMPORTNAMESPACEANDTYPE(N, R)                \
+typedef N::R ## _t R ## _t;                         \
+namespace R                                         \
+{                                                   \
+    using namespace N::R;                           \
 }
 
-#define IMPORTNAMESPACEANDTYPEANDSHIFT(N, R)\
-typedef N::R ## _t R ## _t;                 \
-namespace R                                 \
-{                                           \
-    using namespace N::R;                   \
-    namespace Shift                         \
-    {                                       \
-        using namespace N::R::Shift;        \
-    }                                       \
+#define IMPORTNAMESPACEANDTYPEANDSHIFT(N, R)        \
+typedef N::R ## _t R ## _t;                         \
+namespace R                                         \
+{                                                   \
+    using namespace N::R;                           \
+    namespace Shift                                 \
+    {                                               \
+        using namespace N::R::Shift;                \
+    }                                               \
 }
 
-#define IMPORTNAMESPACE(N, n)               \
-namespace n                                 \
-{                                           \
-    using namespace N::n;                   \
+#define IMPORTNAMESPACE(N, n)                       \
+namespace n                                         \
+{                                                   \
+    using namespace N::n;                           \
+}
+
+#define IMPORTNAMESPACEANDTYPEAS(N, R, A)           \
+typedef N::R ## _t A ## _t;                         \
+namespace A                                         \
+{                                                   \
+    using namespace N::R;                           \
+}
+
+#define IMPORTNAMESPACEANDTYPEANDSHIFTAS(N, R, A)   \
+typedef N::R ## _t A ## _t;                         \
+namespace A                                         \
+{                                                   \
+    using namespace N::R;                           \
+    namespace Shift                                 \
+    {                                               \
+        using namespace N::R::Shift;                \
+    }                                               \
+}
+
+#define IMPORTNAMESPACEAS(N, n, a)                  \
+namespace a                                         \
+{                                                   \
+    using namespace N::n;                           \
 }
